dc08f160123b9e5956ada5c63fd1ffcc640e0059
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2024-05-03 11:17:01 -0300

    feat: Adição de configuração do banco de dados.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Modules:
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule]
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123123',
+      database: 'portfolio_db',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ]
 })
 export class AppModule {}
